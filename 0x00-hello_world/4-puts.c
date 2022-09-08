163876823da5855dfd82cfcf69f667384a7a7f41
BoggyDan <danieladewale2006@gmail.com>
2022-09-08 06:45:26 -0700

    C is not as hard as it seems

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+# include<stdio.h>
+
+int main(void) 
+{
+    char Alx[] = "Programming is like building a multilingual puzzle\n";
+      /* Using the puts command*/
+    puts(Alx);
+
+    return 0;
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include<stdio.h>
+
+int main(void) 
+{
+   char Alx[] = "with proper grammar, but the outcome is a piece of art,\n"  
+  /* Using the printf command*/
+   printf(Alx);
+
+   return 0;
+}
