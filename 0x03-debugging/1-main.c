8daee283c65a470b9cc5a61010f29c8cacef130e
BoggyDan <danieladewale2006@gmail.com>
2022-09-15 13:04:23 +0100

    Fixing files according to betty style

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,47 +1,24 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-  
+/**
+ * main - causes an infinite loop
+ * Return: 0
+ */
 
- /** 
+int main(void)
+{
+	int i;
 
-  * main - causes an infinte loop 
+	printf("Infinite loop incoming :(\n");
 
-  * Return: 0 
+	i = 0;
 
-  */ 
+	/*while (i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
 
-  
+	printf("Infinite loop avoided! \\o/\n");
 
- int main(void) 
-
- { 
-
-         int i; 
-
-  
-
-         printf("Infinite loop incoming :(\n"); 
-
-  
-
-         i = 0; 
-
-  
-
-         /*while (i < 10)*/ 
-
-         /*{*/ 
-
-         /*        putchar(i);*/ 
-
-         /*}*/ 
-
-  
-
-         printf("Infinite loop avoided! \\o/\n"); 
-
-  
-
-         return (0); 
-
- }
+	return (0);
+}
