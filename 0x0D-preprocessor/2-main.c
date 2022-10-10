c844b1617ae84fe3496c3b1a591b44f2f8bfd5a6
BoggyDan <danieladewale2006@gmail.com>
2022-10-10 04:37:11 -0700

    Alx & Betty issues

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
- * main - entry point
+ * main - prints the name of the file.
  *
- * Return: 0
+ * Return: Always 0.
  */
-
 int main(void)
 {
-    printf(__FILE__"\n");
-
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
